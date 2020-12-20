591036cab48531709eaab76015f525de8017568e
yomj1 <mohashaibi1@gmail.com>
2020-12-20 10:36:32 +0200

    State checkpoint

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import image from './logo.svg';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = { 
+    counter: 0 ,
+    person:{
+      fullName: "Mohamed Ashaibi",
+      bio: "This is the bio",
+      imgSrc: image,
+      profession:"Profession"
+    },
+    show: false,
+  };
+
+  incrementCounter() {
+    const { counter } = this.state;
+    this.setState({ counter: counter + 1 });
+  }
+
+  changeShow = () => {
+    this.setState({
+      show: !this.state.show
+    });
+  }
+
+  componentDidMount() {
+    const thisBoundedIncrementer = this.incrementCounter.bind(this);
+    setInterval(thisBoundedIncrementer, 1000);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.show && 
+        <div className="profile-info">
+          <img src={this.state.person.imgSrc} alt="ok"/>
+          <h1>{this.state.person.fullName}</h1>
+          <h3>{this.state.person.profession}</h3>
+          <p>{this.state.person.bio}</p>
+        </div>
+        }
+        <div className="counter">
+          <h1>Counter</h1>
+          <h2>{this.state.counter}</h2>
+        </div>
+        <button onClick={this.changeShow}>okok</button>
+      </div>
+    );
+  }
 }
 
 export default App;
